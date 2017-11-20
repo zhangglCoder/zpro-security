fb598bead696492d78dd7474ab9e30327071d3a1
guolongzhang <guolongzhang@ebnew.com>
2017-11-20 11:41:49 +0800

    重构

---
+++ security-browser/src/main/java/cn/zpro/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,21 @@
+package cn.zpro.security.browser;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zhanggl
+ */
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+               .formLogin()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+}
--- security-demo/src/main/java/cn/zpro/security/controller/DemoController.java
+++ security-demo/src/main/java/cn/zpro/security/controller/DemoController.java
@@ -1,9 +1,12 @@
 package cn.zpro.security.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 /**
  * @author leon
  */
@@ -11,9 +14,27 @@ import org.springframework.web.servlet.ModelAndView;
 public class DemoController {
 
     @GetMapping
-    public ModelAndView index(ModelAndView view){
+    public ModelAndView index(ModelAndView view) throws IOException {
         System.out.println("hello");
         view.setViewName("index");
         return view;
+    }
+    @GetMapping("about")
+    public ModelAndView about(ModelAndView view) throws IOException {
+        System.out.println("about");
+        view.setViewName("about");
+        return view;
+    }
+    @GetMapping("login")
+    public ModelAndView login(ModelAndView view) throws IOException {
+        System.out.println("login");
+        view.setViewName("login");
+        return view;
+    }
+
+    @GetMapping("user")
+    @ResponseBody
+    public Authentication user(Authentication authentication){
+        return authentication;
     }
 }
